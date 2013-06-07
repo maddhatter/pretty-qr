47938e2f3e489f9356b908807c7fa26a96032e2c
Shawn Tunney <shawn.tunney@gmail.com>
2013-06-07 14:40:44 -0400

    First Commit

---
+++ src/Maddhatter/PrettyQr/PrettyQrServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Maddhatter\PrettyQr;
+
+use Illuminate\Support\ServiceProvider;
+
+class PrettyQrServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('maddhatter/pretty-qr');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
